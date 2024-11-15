89ddc0c576e3fa42ad7f44046ab7c4221867ed30
MINAH <tjdns7831@naver.com>
2024-11-15 10:59:50 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/popflix/PopflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.popflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopflixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopflixBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/popflix/handler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.popflix.handler;
+
+import com.popflix.util.ApiUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatusCode statusCode, WebRequest request) {
+        log.error(ex.getMessage(), ex);
+
+        ApiUtil.ApiError<String> error = ApiUtil.error(statusCode.value(), "알 수 없는 오류가 발생했습니다. 문의 바랍니다.");
+        return super.handleExceptionInternal(ex, error, headers, statusCode, request);
+    }
+}
